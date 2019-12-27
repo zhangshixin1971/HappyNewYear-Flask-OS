35a25c2400ce3d97437686e4fafed74f08dc7294
zhangshixin1971 <58924188+zhangshixin1971@users.noreply.github.com>
2019-12-27 08:22:53 -0500

    verion 2

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,25 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_file
 
 application = Flask(__name__)
+
 
 @application.route('/')
 def my_form():
     return render_template('my-form.html')
 
+
+@application.route('/user')
+def user_response():
+    user_name = request.args.get('user')
+    return render_template('return-form.html', value=user_name)
+
+
 @application.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['text']
-    processed_text = text + " Wish you and your family Merry Christmas and Happy New Year!"
-    return processed_text
+    return render_template('return-form.html', value=text)
+
 
 if __name__ == "__main__":
     application.run()
+
